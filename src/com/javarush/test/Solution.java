08b6cfd2dc462e7a478fc8a9a8da8c92818eb95e
Artyom <art231188@gmail.com>
2016-01-19 20:20:01 +0200

    input keyboard stuff

--- src/com/javarush/test/Solution.java
+++ src/com/javarush/test/Solution.java
@@ -1,9 +1,20 @@
 package com.javarush.test;
 
+import java.io.*;
+
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here 777 rwer
-        System.out.print("Start means beginning");
+
+     /*     Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.print(name + "some text");*/
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String name = input.readLine();
+        System.out.print(name + "some text");
+
     }
 }
+
